6fecb4e0be887010621072f7eaf9f53c78809fe2
Bhargavi Bhatia <66355146+Bhargavibhatia215@users.noreply.github.com>
2022-11-15 22:17:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# ChatX App🚀
+
+ ChatX is a real time web chat application. It is a completely free application which enables you to have live chats within seconds.
+Chat with friends, colleagues without log in.
+
+## Tech Stack
+
+**Client Side:** HTML5, CSS, Javascript<br/>
+**Server Side:** Socket.io, ExpressJs, NodeJs
+
+## Future Goal
+
+To make the website responsive. For now only one room is created whose code is A1 to join, this can be improved by generating roomid randomly.
+
+## Deployed Project
+
+ You can checkout my website on laptop from here https://chatx-app-deployed.herokuapp.com/
+ 
+## Screenshots
+
+### Homepage
+![Page1](https://user-images.githubusercontent.com/68382993/152633886-26346d92-92f6-4f39-b697-d10a4e281f2f.PNG)
+### After entering username, user will be given choice to create a room or join an existing room
+![Page2](https://user-images.githubusercontent.com/68382993/152633889-1baf0fb2-eded-4ab9-b842-bdc0590f851c.PNG)
+### If you try to join a room
+![page2_join](https://user-images.githubusercontent.com/68382993/152633892-d383ff76-5440-4de7-b086-09291fd4f9ff.PNG)
+### After creating a new room or joining a new room, user will be directed to following page
+![Page3](https://user-images.githubusercontent.com/68382993/152633897-fd85f7a5-45ec-4983-9d1f-5f539f9767b6.PNG)
+### User1 - Dummyuser screen
+![page4](https://user-images.githubusercontent.com/68382993/152633900-343aa16e-219b-41b7-abc5-90fdcb65f3a7.PNG)
+### User2 - Dummy2 screen
+![page5](https://user-images.githubusercontent.com/68382993/152633903-48ec4e3f-92c1-4a25-b0cc-3fad5aed8095.PNG)
+
+
+## Authors
+
+- [@Shreya Thapliyal](https://www.github.com/ShreyaThapliyal)
+
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at shreya058mca21@igdtuw.ac.in
---
+++ chat.js
@@ -0,0 +1,116 @@
+const socket=io();
+const bottom=document.querySelector('.bottom');
+const outer=document.querySelector('.outer-container');
+const joinbtn=document.querySelector('.btn-join');
+const exitbtn=document.querySelector('.btn-exit');
+const create_room=document.querySelector('.btn-create');
+const waytogo=document.querySelector('.go_button');
+const sendbtn=document.querySelector('.btn-send');
+const message=document.querySelector('.message-box');
+const msgindividual=document.querySelector('.messages-in');
+const name=document.querySelector('.username');
+const homepage=document.querySelector('.home-page');
+const chat=document.querySelector('.chat-page');
+const room=document.querySelector('.roomcondition');
+const jroom=document.querySelector('.join_a_room');
+const rcode=document.querySelector('.code');
+
+
+const roomid=['A1','A2'];
+let uname;
+
+function display(msg,position){
+    const element = document.createElement("div");
+    element.innerText=msg;
+    const currentDiv=document.querySelector(".messages-in");
+    element.classList.add(position);
+    currentDiv.appendChild(element);
+};
+
+message.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('.btn-send').click();
+    }
+});
+
+joinbtn.addEventListener("click",function(){
+    if(name.value===""){
+        return;
+    }
+    uname=name.value;
+    homepage.classList.add("hidden");
+    room.classList.remove("hidden");
+    name.value=''; 
+    // outer.scrollTop = outer.scrollHeight;
+    // used in future so keep a check maybe try to have value of name in another value and use it for remaining code or figure out any other way
+});
+
+create_room.addEventListener("click",function(){
+    room.classList.add("hidden");
+    chat.classList.remove("hidden");
+    msgindividual.classList.remove("hidden");
+    socket.emit('new-user-joined',uname);
+    socket.on('joined',(name)=>{
+        //Display message tht new user has joined
+        display(`${name} has joined the conversation`,'other');
+    });
+    outer.style.height="71.5vh";
+    outer.style.marginBottom="80px";
+    bottom.classList.remove("hidden");
+});
+
+waytogo.addEventListener("click",function(){
+    if(rcode.value==roomid[0]){
+        jroom.classList.add('hidden');
+        chat.classList.remove('hidden');
+        bottom.classList.remove("hidden");
+        outer.style.height="71.5vh";
+        outer.style.marginBottom="80px";
+        outer.scrollTop = outer.scrollHeight;
+        socket.emit('new-user-joined',uname);
+        socket.on('joined',(name)=>{
+        //Display message tht new user has joined
+        display(`${name} has joined the conversation`,'other');
+        outer.scrollTop = outer.scrollHeight;
+        });
+    }
+    else{
+        alert("Enter valid code");
+    }
+});
+
+exitbtn.addEventListener("click",function(){
+    chat.classList.add("hidden");
+    homepage.classList.remove("hidden");
+    msgindividual.classList.add("hidden");
+    bottom.classList.add("hidden");
+    socket.emit('user-exited');
+    outer.style.height="80vh";
+    outer.style.marginBottom="20px";
+    outer.scrollTop = outer.scrollHeight;
+});
+
+socket.on('leave',(idname)=>{
+  //username has left the conversation
+  display(`${idname} has left the conversation`,'other');
+  outer.scrollTop = outer.scrollHeight;
+});
+
+socket.on('recieve',data=>{
+  // Display the message along with username
+  display(`${data.name} : ${data.message}`,'other');
+  outer.scrollTop = outer.scrollHeight;
+});
+
+sendbtn.addEventListener("click",function(){
+    if(message.value===""){
+        return;
+    }
+    //Sockets part
+    const messageInput=message.value;
+    display(`You : ${messageInput}`,'self');
+    outer.scrollTop = outer.scrollHeight;
+    socket.emit('send',messageInput);
+    message.value='';
+});
